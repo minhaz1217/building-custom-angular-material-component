3f09f62d38672f80d10e49c515ed0f5abc42f287
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-12-10 14:34:05 +0600

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhoneNumberPickerComponent } from "./phone-number-picker/phone-number-picker.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PhoneNumberPickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
