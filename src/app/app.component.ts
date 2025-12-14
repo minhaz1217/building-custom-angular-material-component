1e6c18a6379963e9935a1425a6ca7f0c07e6d37b
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-12-14 23:32:52 +0600

    trying out the form field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PhoneNumberPickerComponent } from "./phone-number-picker/phone-number-picker.component";
+import { PhoneNumberPickerComponent } from './phone-number-picker/phone-number-picker.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PhoneNumberPickerComponent],
+  imports: [
+    RouterOutlet,
+    PhoneNumberPickerComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'building-custom-angular-material-component';
