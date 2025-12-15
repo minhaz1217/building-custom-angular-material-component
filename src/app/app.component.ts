7c89b079eb5da6c3c2358fa4619a3618e0f02844
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-12-15 12:12:17 +0600

    angular material working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PhoneNumberPickerComponent } from './phone-number-picker/phone-number-picker.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { CommonModule } from '@angular/common';
+import { FormFieldCustomControlExample } from './example-tel-input/example-tel-input.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,10 +21,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
+    CommonModule,
+    FormFieldCustomControlExample,
+    MatSlideToggleModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'building-custom-angular-material-component';
+  readonly form = new FormGroup({
+    tel: new FormControl(null),
+  });
 }
